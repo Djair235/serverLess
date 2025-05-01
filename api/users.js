4305a17dcff39c89722f422823320a5d8166b81e
Djair235 <djairdecastrofeitosa@gmail.com>
2025-05-01 19:09:31 -0300

    .

--- api/users.js
+++ api/users.js
@@ -3,6 +3,14 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default async function apiResponse(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+      }
+
     if (req.method === "GET") {
         const users = await prisma.usuario.findMany()
         return res.status(200).json(users)
