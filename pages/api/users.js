7535960a18329e95e8438b6ad51d8ff2c799f8fa
Djair235 <djairdecastrofeitosa@gmail.com>
2025-05-01 16:09:59 -0300

    .

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,3 +1,3 @@
 export default async function apiResponse(req, res) {
-    res.status(200).json({message: "bom dia boa tarde boa noite"})
+    res.status(200).json({message: "bom dia boa tarde boa noite"});
 }
