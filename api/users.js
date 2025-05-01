574f3ecfb7998522f409206735a26241b720aef5
Djair235 <djairdecastrofeitosa@gmail.com>
2025-05-01 17:44:18 -0300

    .

--- api/users.js
+++ api/users.js
@@ -4,9 +4,8 @@ const prisma = new PrismaClient()
 
 export default async function apiResponse(req, res) {
     if (req.method === "GET") {
-        const message = "bom dia boa tarde boa noite"
         const users = await prisma.usuario.findMany()
-        return res.status(200).json({ message, users })
+        return res.status(200).json({ users })
     }
 
     return res.status(400).json({ error: "Método não permitido. Use GET." })
